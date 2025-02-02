6b512d1e0575e6cabfe8fffbac6a40bd42eda403
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-02-02 11:20:16 +0300

    Added teacher solution for task: Задание: Реализация CRUD для задач

---
+++ 05-db/01-crud-tasks/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from "@nestjs/common";
+import { TasksModule } from "./tasks/tasks.module";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Task } from "./tasks/entities/task.entity";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "db.sqlite",
+      entities: [Task],
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
+})
+export class AppModule {}
